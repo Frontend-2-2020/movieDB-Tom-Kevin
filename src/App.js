b1d8f52401445b70135edb7d49f1a1e9068d3c8f
Tom Derdaele <t.derdaele@goekint.be>
2020-01-06 21:06:10 +0100

    aanpassing dubbele react verwijzing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import './App.css';
-
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+    <div>
+      
+    </div>);
   }
 }
