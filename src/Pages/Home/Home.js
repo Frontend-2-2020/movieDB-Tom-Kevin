51276848d8a24b036dc3aec8f59df4add2db0795
Kevin Vanantwerpen <kevin.v1989@gmail.com>
2020-01-06 21:57:26 +0100

    packages installed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { KEY } from '../../config';
+import axios from 'axios';
 
 class Home extends Component {
+  getData = () =>{
+    axios
+      .get("https://uselessfacts.jsph.pl/random.json?language=en")
+      .then(response => {
+        console.log(response);
+        });
+      
+  }
+
   render() {
+    this.getData();
     console.log(KEY)
     return (
       <div>
