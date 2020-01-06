157fab098980bae12d8bf3b9ef1ce34b5ee4cd79
Tom Derdaele <t.derdaele@goekint.be>
2020-01-06 21:32:16 +0100

    Css files appart in map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Detail from "./Pages/Detail/Detail";
 
 export default class App extends Component {
   render() {
     return (
     <div>
-      
+      <Detail/>
     </div>);
   }
 }
