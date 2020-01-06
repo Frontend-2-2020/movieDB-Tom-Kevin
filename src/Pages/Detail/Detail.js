a2569d9e4b565a421ce4f8cff2952d1d3973e8cc
Tom Derdaele <t.derdaele@goekint.be>
2020-01-06 21:33:24 +0100

    Opnieuw

---
+++ src/Pages/Detail/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from "./Detail.module.css";
+
+export default function Detail() {
+  return (
+    <div className={classes.button}>
+      Test
+      
+    </div>
+  )
+}
