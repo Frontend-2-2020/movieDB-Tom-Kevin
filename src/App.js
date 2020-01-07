fb7dd2bf2946bb05538d1703ce0a10c07f2482ea
Tom Derdaele <t.derdaele@goekint.be>
2020-01-07 23:04:54 +0100

    Navbar gemaakt, en gezet in app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Detail from "./Pages/Detail";
 import Home from "./Pages/Home";
+import NavBar from "./Components/NavBar";
 
 export default class App extends Component {
   render() {
     return (
         <Router>
+          <NavBar/>
           <Switch>
             <Route path="XXXX" component={Detail}/>
             <Route path="/" component={Home}/>
