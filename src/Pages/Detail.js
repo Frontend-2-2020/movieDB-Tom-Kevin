57d96b164e027148774d8d00a872c800cd300f6a
Tom Derdaele <t.derdaele@goekint.be>
2020-01-18 10:27:38 +0100

    Code wat verfijnd om conditioneel te renderen.

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,33 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Axios from "axios";
 import { KEY } from "../config";
-import CompanyLogo from '../Components/CompanyLogo';
-import Genre from '../Components/Genre';
-
+import CompanyLogo from "../Components/CompanyLogo";
+import Genre from "../Components/Genre";
 
 export default class Detail extends Component {
   state = {
-    movie:[],
-    compagnie: [],
-    genres: []
+    movie: ""
   };
 
   componentDidMount() {
-    Axios.get(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${KEY}`)
-      .then(response => {
-        this.setState({
-          compagnie: response.data.production_companies,
-          genres: response.data.genres,
-          movie: response.data
-        });
+    Axios.get(
+      `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${KEY}`
+    ).then(response => {
+      this.setState({
+        movie: response.data
       });
+    });
   }
 
   render() {
+    // onderstaande code is nodig omdat er anders geen .map kan worden gedaan bij initial mount
+    let nestedRender;
+    if (this.state.movie) {
+      nestedRender = (
+        <div>
+          <div className="logos mt-4">
+            {this.state.movie.production_companies.map(post => (
+              <CompanyLogo key={post.id} logo_path={post.logo_path} />
+            ))}
+          </div>
+          <div className="logos mt-4">
+            {this.state.movie.genres.map(genre => (
+              <Genre key={genre.id} genre={genre.name} />
+            ))}
+          </div>{" "}
+        </div>
+      );
+    }
+
     return (
       <div id="detail" style={{ display: "block" }}>
-        <div className="jumbotron" style={{backgroundImage: `url('https://image.tmdb.org/t/p/original${this.state.movie.backdrop_path}')`}}>
-          <h1 className="display-4">
+        <div
+          className="jumbotron"
+          style={{
+            backgroundImage: `url('https://image.tmdb.org/t/p/original${this.state.movie.backdrop_path}')`
+          }}
+        >
+          <h1 className="display-4 text-light">
             <span id="title">{this.state.movie.title}</span>{" "}
             <span className="badge badge-success" id="score">
               {this.state.movie.vote_average}
@@ -41,17 +61,7 @@ export default class Detail extends Component {
                 <div className="card-body">
                   <p id="samenvatting">{this.state.movie.overview}</p>
 
-                  <div className="logos mt-4">
-                      {this.state.compagnie.map(post => (
-                        <CompanyLogo key={post.id} logo_path={post.logo_path}/>
-                      ))}
-                  </div>
-
-                  <div className="logos mt-4">
-                      {this.state.genres.map(genre => (
-                        <Genre key={genre.id} genre={genre.name}/>
-                      ))}
-                  </div>
+                  {nestedRender}
 
                   <p className="text-muted mt-4">
                     Release: {this.state.movie.release_date}
@@ -73,6 +83,6 @@ export default class Detail extends Component {
           </div>
         </div>
       </div>
-    ); 
+    );
   }
 }
